e2b8490452a0e60b68dea1e719b17020267289ad
pd3v <paulo.develop@gmail.com>
2024-04-20 17:41:33 +0100

    add formula

--- line.rb
+++ line.rb
@@ -5,12 +5,12 @@ class Line < Formula
   sha256 "cd2e09f2fd0aa43fbfef5d1c0ca9300765c21703e7e366e566c9f35c18144878"
   license "MIT"
 
-  # depends_on "cmake" => :build
+  depends_on "cmake" => :build
   depends_on "readline" => :recommended
 
   def install
     system "mkdir", "build"
-    #system "mkdir", "bin"
+    system "mkdir", "bin"
     system "cmake", "-S", ".", "-B", "build/" , *std_cmake_args
     system "make", "-C", "build/"
     bin.install "build/line"
