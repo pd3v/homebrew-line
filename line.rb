86bc233c502d6bacd49453d502997fa2bdf4184f
pd3v <paulo.develop@gmail.com>
2024-04-30 17:51:59 +0100

    get submodules testing

--- line.rb
+++ line.rb
@@ -1,8 +1,8 @@
 class Line < Formula
   desc "tiny command-line midi sequencer and language for live coding. ableton link compatible."
   homepage "https://github.com/pd3v/line"
-  head "https://github.com/pd3v/line.git"
-  #, :branch => :ableton_link_integration
+  head "https://github.com/pd3v/line.git", :branch => :ableton_link_integration
+
   #url "https://github.com/pd3v/line/archive/refs/tags/v0.6.1.tar.gz"
   #sha256 "cd2e09f2fd0aa43fbfef5d1c0ca9300765c21703e7e366e566c9f35c18144878"
   #license "MIT"
@@ -11,8 +11,8 @@ class Line < Formula
   #url "https://github.com/Ableton/link.git", tag: "Link-3.1.0", revision: "e55789a3f665de4416dc4809e4a8934266110ffd"
 
   depends_on "cmake"  =>  :build
-  #depends_on "readline" =>  :recommended
-  depends_on "readline" =>  :optional
+  depends_on "readline" =>  :recommended
+  #depends_on "readline" =>  :optional
   #depends_on "rtmidi" =>  :build
   #depends_on "link" =>  :build
 
