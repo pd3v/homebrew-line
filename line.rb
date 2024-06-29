3ddbf6347a3fdd92d4a44a906213446a165afa90
pd3v <paulo.develop@gmail.com>
2024-06-29 16:51:51 +0100

    overall refactoring

--- line.rb
+++ line.rb
@@ -1,23 +1,34 @@
 class LineAT062 < Formula
-  desc "tiny command-line midi sequencer and language for live coding music. < ableton link compatible >"
+  desc "Tiny command-line midi sequencer and language for live coding music. ableton link compatible"
   homepage "https://github.com/pd3v/line"
-  #url "https://github.com/pd3v/line/archive/refs/tags/v0.6.2.tar.gz"
-  head "https://github.com/pd3v/line", :branch => "ableton_link_integration"
+  url "https://github.com/pd3v/line",
+  tag: "v0.6.2"
+ 
   sha256 "3a380903d4abb79dd889c3e3d5d380563c01f1b1530ef6258c65223d6d34364c"
   license "MIT"
 
-  depends_on "cmake" => :recommend
   depends_on "readline" => :recommended
-
+  depends_on "cmake" => :build
+  
   def install
     #system "./configure", "--disable-silent-rules", *std_configure_args
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
     system "mkdir", "build"
     system "mkdir", "bin"
     system "cmake", "-S", ".", "-B", "build/" , *std_cmake_args
     system "make", "-C", "build/"
+
+    on_macos do
+      lib.install "build/externals/lpeg/liblpeg.dylib"
+      lib.install "build/externals/lua/liblua.dylib"
+      lib.install "build/externals/lua/librtmidi.dylib"
+    end
+    on_linux do
+      lib.install "build/externals/lpeg/liblpeg.so"
+      lib.install "build/externals/lua/liblua.so"
+      lib.install "build/externals/lua/librtmidi.so"
+    end
     bin.install "line"
-    #system "./build.sh"  
+  
     ohai("Make some noise!")
   end
 end
